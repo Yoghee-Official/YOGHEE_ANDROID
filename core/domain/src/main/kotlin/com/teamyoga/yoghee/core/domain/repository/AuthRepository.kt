45d7bf54c72999cf304c094abd5bfed2444d2956
yunjin-park97 <97.yunjin@gmail.com>
2026-06-08 00:42:22 +0900

    [feature] 자동로그인 구현

--- core/domain/src/main/kotlin/com/teamyoga/yoghee/core/domain/repository/AuthRepository.kt
+++ core/domain/src/main/kotlin/com/teamyoga/yoghee/core/domain/repository/AuthRepository.kt
@@ -1,8 +1,18 @@
 package com.teamyoga.yoghee.core.domain.repository
 
+import com.teamyoga.yoghee.core.domain.model.AuthState
 import com.teamyoga.yoghee.core.domain.model.SsoLoginResult
 import com.teamyoga.yoghee.core.domain.model.SsoType
+import kotlinx.coroutines.flow.StateFlow
 
 interface AuthRepository {
+    val authState: StateFlow<AuthState>
+
+    fun currentAccessToken(): String?
+
     suspend fun ssoLogin(token: String, ssoType: SsoType): SsoLoginResult
+
+    suspend fun refreshAccessToken(): Boolean
+
+    suspend fun logout()
 }

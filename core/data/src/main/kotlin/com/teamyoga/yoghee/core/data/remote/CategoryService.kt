f50b61a532480611c69eb3ecb9ba38d9ce85232e
yunjin-park97 <97.yunjin@gmail.com>
2026-06-19 00:46:28 +0900

    [feature] 취향탐색 화면 개발

---
+++ core/data/src/main/kotlin/com/teamyoga/yoghee/core/data/remote/CategoryService.kt
@@ -0,0 +1,12 @@
+package com.teamyoga.yoghee.core.data.remote
+
+import com.teamyoga.yoghee.core.data.remote.model.CategoryClassResponse
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface CategoryService {
+    @GET("api/class/category/{categoryId}")
+    suspend fun getClassesByCategory(
+        @Path("categoryId") categoryId: String,
+    ): CategoryClassResponse
+}

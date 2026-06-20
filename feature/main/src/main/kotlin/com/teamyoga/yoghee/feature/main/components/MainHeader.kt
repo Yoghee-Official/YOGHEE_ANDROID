1b4ce3da1e203c628f4f4d6e48233828356429e9
yunjin-park97 <97.yunjin@gmail.com>
2026-06-21 00:55:58 +0900

    [feature] 취향탐색 화면 개발

--- feature/main/src/main/kotlin/com/teamyoga/yoghee/feature/main/components/MainHeader.kt
+++ feature/main/src/main/kotlin/com/teamyoga/yoghee/feature/main/components/MainHeader.kt
@@ -4,8 +4,6 @@ import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -25,7 +23,6 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -40,6 +37,7 @@ import androidx.compose.ui.unit.sp
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.draw.shadow
 import com.teamyoga.yoghee.core.ui.R
+import com.teamyoga.yoghee.core.ui.util.noRippleClickable
 import com.teamyoga.yoghee.feature.main.TrainingType
 
 @Composable
@@ -136,12 +134,7 @@ fun CategoryToggle(
                     modifier = Modifier
                         .weight(1f)
                         .fillMaxHeight()
-                        .clickable(
-                            indication = null,
-                            interactionSource = remember { MutableInteractionSource() }
-                        ) {
-                            onTrainingTypeChanged(type)
-                        },
+                        .noRippleClickable { onTrainingTypeChanged(type) },
                     contentAlignment = Alignment.Center
                 ) {
                     Text(

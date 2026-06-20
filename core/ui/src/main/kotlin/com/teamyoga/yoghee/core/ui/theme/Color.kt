f9daf3ae765b02c4699e89c5dc4c87bfecb2f998
yunjin-park97 <97.yunjin@gmail.com>
2026-06-21 00:56:07 +0900

    [feature] 취향탐색 화면 개발

--- core/ui/src/main/kotlin/com/teamyoga/yoghee/core/ui/theme/Color.kt
+++ core/ui/src/main/kotlin/com/teamyoga/yoghee/core/ui/theme/Color.kt
@@ -9,8 +9,6 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
-
-val Background = Color(0xFFEFEDEB)
 val Green_D6F695 = Color(0xFFD6F695)
 
 val SAND_BEIGE = Color(0xFFFCFAF4)

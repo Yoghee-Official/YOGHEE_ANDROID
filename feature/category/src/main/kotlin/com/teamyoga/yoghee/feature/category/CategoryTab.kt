d45cc73423098a74e10e00d91c9554e02a680e82
yunjin-park97 <97.yunjin@gmail.com>
2026-06-21 13:59:51 +0900

    [feature] 위치탐색 화면 개발

--- feature/category/src/main/kotlin/com/teamyoga/yoghee/feature/category/CategoryTab.kt
+++ feature/category/src/main/kotlin/com/teamyoga/yoghee/feature/category/CategoryTab.kt
@@ -12,3 +12,14 @@ internal val CategoryTabs: List<CategoryTab> = listOf(
     CategoryTab(id = "4", name = "이색요가"),
     CategoryTab(id = "5", name = "전통요가"),
 )
+
+internal val LocationTabs: List<CategoryTab> = listOf(
+    CategoryTab(id = "서울", name = "서울"),
+    CategoryTab(id = "경기도", name = "경기도"),
+    CategoryTab(id = "경상도", name = "경상도"),
+    CategoryTab(id = "강원도", name = "강원도"),
+    CategoryTab(id = "전라도", name = "전라도"),
+    CategoryTab(id = "충청도", name = "충청도"),
+    CategoryTab(id = "제주도", name = "제주도"),
+    CategoryTab(id = "기타", name = "기타"),
+)

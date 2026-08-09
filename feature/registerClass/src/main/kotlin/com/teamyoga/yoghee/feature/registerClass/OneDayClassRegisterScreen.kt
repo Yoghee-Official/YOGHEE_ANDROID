97bff1d08853ac2083907240fe62a67187e38b39
yunjin-park97 <97.yunjin@gmail.com>
2026-08-09 19:52:49 +0900

    [feature] 클래스 등록 신규 개발 - 신규 장소 등록

--- feature/registerClass/src/main/kotlin/com/teamyoga/yoghee/feature/registerClass/OneDayClassRegisterScreen.kt
+++ feature/registerClass/src/main/kotlin/com/teamyoga/yoghee/feature/registerClass/OneDayClassRegisterScreen.kt
@@ -24,6 +24,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -153,7 +154,7 @@ private fun OneDayClassRegisterContent(
     onErrorConsumed: () -> Unit,
     modifier: Modifier = Modifier,
 ) {
-    var currentStep by remember { mutableIntStateOf(1) }
+    var currentStep by rememberSaveable { mutableIntStateOf(1) }
     val snackbarHostState = remember { SnackbarHostState() }
     val isLoading = state.submitState is SubmitState.Loading
 

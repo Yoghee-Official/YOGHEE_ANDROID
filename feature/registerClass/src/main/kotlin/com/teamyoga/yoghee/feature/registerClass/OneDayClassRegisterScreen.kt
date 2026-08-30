074de09b085423082c533e2b05e039e0d334acc7
yunjin-park97 <97.yunjin@gmail.com>
2026-08-30 17:41:34 +0900

    [feature] 하루수련 금액 등록

--- feature/registerClass/src/main/kotlin/com/teamyoga/yoghee/feature/registerClass/OneDayClassRegisterScreen.kt
+++ feature/registerClass/src/main/kotlin/com/teamyoga/yoghee/feature/registerClass/OneDayClassRegisterScreen.kt
@@ -23,10 +23,15 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.ime
+import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.union
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
@@ -216,7 +221,9 @@ private fun OneDayClassRegisterContent(
         Column(
             modifier = Modifier
                 .fillMaxSize()
-                .navigationBarsPadding()
+                .windowInsetsPadding(
+                    WindowInsets.navigationBars.union(WindowInsets.ime),
+                )
                 .background(SAND_BEIGE),
         ) {
             Box(modifier = Modifier.weight(1f)) {
